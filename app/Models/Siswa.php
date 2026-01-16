a22e406beaceffdbd6ea34b53cf451f9c0842a95
Aamyoo <ishaqhaqis@gmail.com>
2026-01-16 14:42:03 +0700

    materi/tugas 3.2

---
+++ app/Models/Siswa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    protected $fillable = [
+'nisn',
+'nama_siswa',
+'jenis_kelamin',
+'kelas_id'
+];
+}
